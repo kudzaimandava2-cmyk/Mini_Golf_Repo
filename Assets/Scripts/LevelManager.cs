687f71117687cae8cd89dd4d19cf4b5896e2666d
kudzaimandava2-cmyk <kudzaimandava2@gmail.com>
2025-11-22 09:03:44 -0500

    commit these

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private PlayerRecord playerRecord;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+   
+}
