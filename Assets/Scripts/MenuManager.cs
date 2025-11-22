dd85066f1b9f3b6d0639be064bcda648a1c96a4f
kudzaimandava2-cmyk <kudzaimandava2@gmail.com>
2025-11-21 19:25:09 -0500

    commit

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,10 +7,16 @@ public class MenuManager : MonoBehaviour
     public TMP_InputField inputPlayerName;
     public PlayerRecord playerRecord;
     public Button buttonStart;
+    public Button buttonAddPlayer;
 
     public void ButtonAddPlayer()
     {
         playerRecord.AddPlayer(inputPlayerName.text);
         buttonStart.interactable = true;
+        inputPlayerName.text = "";
+        if (playerRecord.playerList.Count == playerRecord.playerColours.Length)
+        {
+            buttonAddPlayer.interactable = false;
+        }
     }
 }
