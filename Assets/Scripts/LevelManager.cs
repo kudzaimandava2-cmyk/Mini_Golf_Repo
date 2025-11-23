4fbf5325a99bbac27ca78ac56fcf0ee4b92dcd43
kudzaimandava2-cmyk <kudzaimandava2@gmail.com>
2025-11-23 13:14:34 -0500

    Update LevelManager.cs

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -21,11 +21,13 @@ public class LevelManager : MonoBehaviour
 
     private void SetupPlayer()
     {
+        // Set up ball color
         if (ball != null && playerIndex < playerRecord.playerColours.Length)
         {
             ball.SetupBall(playerRecord.playerColours[playerIndex]);
         }
 
+        // Set player name
         if (labelPlayerName != null && playerIndex < playerRecord.playerList.Count)
         {
             labelPlayerName.ForceMeshUpdate(); // TMP-safe initialization
@@ -37,26 +39,22 @@ public class LevelManager : MonoBehaviour
     {
         if (playerRecord == null) return;
 
+        // Save previous player's putts
         playerRecord.AddPutts(playerIndex, previousPutts);
 
+        // Move to next player
         if (playerIndex < playerRecord.playerList.Count - 1)
         {
             playerIndex++;
-
-            if (ball != null && playerIndex < playerRecord.playerColours.Length)
-                ball.SetupBall(playerRecord.playerColours[playerIndex]);
-
-            if (labelPlayerName != null && playerIndex < playerRecord.playerList.Count)
-            {
-                labelPlayerName.ForceMeshUpdate();
-                labelPlayerName.text = playerRecord.playerList[playerIndex].name;
-            }
+            SetupPlayer(); // Correctly sets up ball and player UI
         }
         else
         {
+            // Last player: move to next level or scoreboard
             if (playerRecord.levelIndex >= playerRecord.levels.Length - 1)
             {
                 Debug.Log("Scoreboard");
+                // TODO: Load scoreboard scene here
             }
             else
             {
